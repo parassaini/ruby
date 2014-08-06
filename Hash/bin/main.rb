4e6ab18f69230c0e2c5408020930819767fe6f52
paras <paras.saini1992@gmail.com>
2014-08-06 19:30:38 +0530

    Change the name of method from count_character to count_character_in_hash

--- Hash/bin/main.rb
+++ Hash/bin/main.rb
@@ -2,5 +2,5 @@ require_relative "../lib/string"
 
 puts "Enter String"
 string = gets
-hash = string.chomp!.count_characters
+hash = string.chomp!.count_characters_in_hash
 puts hash
--- Hash/lib/string.rb
+++ Hash/lib/string.rb
@@ -1,5 +1,5 @@
 class String
-  def count_characters
+  def count_characters_in_hash
     temp = Hash.new(0)
     each_char{|c| temp[c] +=1}
     temp
