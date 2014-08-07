6e29933f71409365eb5086e9cc93622f4b5b0367
paras <paras.saini1992@gmail.com>
2014-08-07 14:29:32 +0530

    Add condition to incoparate array and hash and optimize code

--- Array_hash/bin/main.rb
+++ Array_hash/bin/main.rb
@@ -1,4 +1,4 @@
 require_relative "../lib/array"
 
-a = ["paras", "saini", "vinsol", "vinayak", 123553]
+a = ["paras", "saii", "vinsol", "vinayak", 123553, [1,2,3,4], {1=> 2, 2=>3, 3=> 4, 4=>5} ]
 puts a.to_hash
--- Array_hash/lib/array.rb
+++ Array_hash/lib/array.rb
@@ -1,9 +1,15 @@
 class Array
+
   def to_hash
     temp = Hash.new { |hash, key| hash[key] = [] }
-    for i in 0...length
-      temp[(self[i].to_s).length] <<= self[i]
+    for element in self
+      if element.kind_of?(Array) || element.kind_of?(Hash)
+        temp[element.length] <<= element
+      else
+        temp[(element.to_s).length] <<= element
+      end
     end
     temp
   end
+
 end
