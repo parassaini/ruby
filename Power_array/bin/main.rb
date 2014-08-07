3ddd80ace9cfd89e60f8965721b9d79f71acd32d
paras <paras.saini1992@gmail.com>
2014-08-07 15:45:07 +0530

    Fix intentation issues

--- Power_array/bin/main.rb
+++ Power_array/bin/main.rb
@@ -1,7 +1,5 @@
 require_relative "../lib/array"
 
-array = 1,2,3,4,5
+array = [1, 2, 3, 4, 5]
 print "Enter power: "
-power = gets.to_i
-array.power(power)
-print array
+print array.power(gets.to_i)
